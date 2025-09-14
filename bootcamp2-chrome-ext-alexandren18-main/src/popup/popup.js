f4315484db47b1eb4632f8bbb95f147e35b05110
VitorPF01 <vitor.pf@sempreceub.com>
2025-09-14 15:09:54 -0300

    Delete bootcamp2-chrome-ext-alexandren18-main directory

--- bootcamp2-chrome-ext-alexandren18-main/src/popup/popup.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('ping');
-const statusEl = document.getElementById('status');
-
-btn.addEventListener('click', async () => {
-  statusEl.textContent = 'Enviando...';
-  const res = await chrome.runtime.sendMessage({ type: 'PING' });
-  statusEl.textContent = `Background respondeu: ${res.time}`;
-});
